84f41451cfeb2b288fd704dd15fd9941ff67e037
Vasantha Kumar <kvasanth373@gmail.com>
2024-06-11 17:29:27 +0530

    updated the about section

--- components/About/About.js
+++ components/About/About.js
@@ -27,21 +27,23 @@ const About = ({ aboutMeText }) => {
       <section
          id="about"
          className="py-16 bg-gradient-to-r from-gray-800 via-gray-900 to-black">
-         <div className="container mx-auto">
+         <div className="container mx-auto w-full">
             <h2 className="text-4xl font-bold mb-8 text-center text-white shadow-text font-montserrat">
                ABOUT
             </h2>
-            <div className="grid md:grid-cols-2 gap-8 items-center">
+            <div className="grid md:grid-cols-1 gap-8 items-center">
                <div>
                   {showTerminal ? (
                      <Terminal aboutMeText={aboutMeText} />
                   ) : (
-                     <p className="text-md leading-relaxed md:text-xl lg:text-lg p-2 font-montserrat">
-                        {aboutMeText}
+                     <p className="text-md leading-relaxed md:text-xl lg:text-large p-2 font-montserrat italic bold">
+                        {
+                           "Full-Stack Software Engineer with 5+ years of experience building and deploying scalable web applications using Python, Go, Rust, and JavaScript frameworks. Adept at DevOps practices for seamless CI/CD pipelines and Linux administration. Passionate about tackling real-world challenges through innovative software solutions."
+                        }
                      </p>
                   )}
 
-                  <ul className="mt-4 text-white flex flex-wrap gap-4">
+                  <ul className="mt-4 text-white flex flex-wrap gap-4 w-full justify-center">
                      <li className="flex items-center justify-center w-12 rounded-full cursor-pointer">
                         <FaTerminal
                            onClick={() => {
@@ -73,13 +75,13 @@ const About = ({ aboutMeText }) => {
                      </li>
                   </ul>
                </div>
-               <div className="max-w-md mx-auto image-container">
+               {/* <div className="max-w-md mx-auto image-container">
                   <img
                      src="/images/profile.jpg"
                      alt="Vasantha Kumar"
                      className="shadow-lg transition rounded-full w-96 h-96 object-cover object-center hover:scale-110  duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 cursor-pointer"
                   />
-               </div>
+               </div> */}
             </div>
          </div>
       </section>
